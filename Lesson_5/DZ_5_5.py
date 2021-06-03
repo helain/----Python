5d5120e43a975ec67d1e30cbcff63c336b4c4fe0
Mikhail <shadow.fist666@gmail.com>
2021-06-03 12:24:07 +0300

    Задача 5

--- Lesson_5/DZ_5_5.py
+++ Lesson_5/DZ_5_5.py
@@ -3,5 +3,13 @@
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
 
-with open('DZ_5_5_example.txt', '+') as user_numbers:
+with open('DZ_5_5_example.txt', 'w') as user_numbers:
     user_numbers.write(input('введите набор чисел через пробел: '))
+
+with open('DZ_5_5_example.txt') as user_numbers:
+    for line in user_numbers:
+        numbers = line.split()
+        user_sum = 0
+        for i in numbers:
+            user_sum += int(i)
+        print(user_sum)
